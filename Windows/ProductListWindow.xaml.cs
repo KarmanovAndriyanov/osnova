13baf1a7d2d13b17c75525d6282b5b5306ef7e6c
user <vladimirgailov@gmail.com>
2023-02-20 15:11:37 +0300

    Добавьте файлы проекта.

---
+++ Windows/ProductListWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace osnova.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListWindow.xaml
+    /// </summary>
+    public partial class ProductListWindow : Window
+    {
+        public ProductListWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LvProduct_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnMore_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Windows/RegistrWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using osnova.DB;
+using osnova.ClassHelper;
+
+namespace osnova.Windows
+{
+    public partial class RegistrWindow : Window
+    {
+        public RegistrWindow()
+        {
+            InitializeComponent();
+            GenderBox.SelectedIndex = 0;
+            GenderBox.DisplayMemberPath = "Name";
+            GenderBox.ItemsSource = EFClass.Context.Gender.ToList();
+
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DoneButto_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(LoginBox.Text))
+            {
+                MessageBox.Show("Логин не может быть пустым или состоять из пробелов");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(NameBox.Text))
+            {
+                MessageBox.Show("Имя не может быть пустым или состоять из пробелов");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(SurnameBox.Text))
+            {
+                MessageBox.Show("Фамилия не может быть пустым или состоять из пробелов");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(SurnameBox.Text))
+            {
+                MessageBox.Show("Фамилия не может быть пустым или состоять из пробелов");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(PasswordBox.Password))
+            {
+                MessageBox.Show("Пароль не может быть пустым или состоять из пробелов");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(NumberBox.Text))
+            {
+                MessageBox.Show("Номер не может быть пустым или состоять из пробелов");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(EmailBox.Text))
+            {
+                MessageBox.Show("Почта не может быть пустым или состоять из пробелов");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(BirthdayBox.Text))
+            {
+                MessageBox.Show("Дата рождения не может быть пустым или состоять из пробелов");
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(GenderBox.Text))
+            {
+                MessageBox.Show("Пол не может быть пустым или состоять из пробелов");
+                return;
+            }
+
+            EFClass.Context.User.Add(new User()
+            {
+                Login = LoginBox.Text,
+                Password = PasswordBox.Password,
+                LastName = SurnameBox.Text,
+                FirstName = NameBox.Text,
+                IdRole = 2,
+
+                Mail = EmailBox.Text,
+                Phone = NumberBox.Text,
+                Birthday = BirthdayBox.SelectedDate.Value,
+                IdGender = (GenderBox.SelectedItem as Gender).IdGender,
+
+            });
+            EFClass.Context.SaveChanges();
+            MessageBox.Show("Ok");
+
+        }
+    }
+}
+
